b5993902a93d97011cab37111599f0f89e877d07
aklesh yadav <arzunayavadu998@gmail.com>
2024-07-20 13:43:38 +0545

    ui design

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,10 +3,31 @@ import React from 'react'
 const Manager = () => {
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">
-                
+            <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+
+            <div className=" max-w-4xl text-white mx-auto b-slate-400 p-4 rounded-3xl">
+                <h1 className='font-bold text-4xl text-center text-green-400'>
+                    <span className='text-green-700'>&lt;</span>
+                    Pass
+                    <span className='text-green-700'>OP/&gt;</span>
+                </h1>
+                <p className='text-green-900 font-bold text-center text-lg'>Your own Password Manager</p>
+                <div className="flex flex-col items-center gap-8 text-black">
+                    <input placeholder='Enter Website URL' className='px-4 py-1 rounded-full border border-green-400 w-full focus:outline-none focus:border-green-600' type="text" name="" id="" />
+                    <div className='flex gap-4 w-full'>
+                        <input placeholder='Enter Username' className='px-4 py-1 rounded-full border border-green-400 w-full focus:outline-none focus:border-green-600' type="text" name="" id="" />
+                        <input placeholder='Enter Password' className='px-4 py-1 rounded-full border border-green-400 w-full focus:outline-none focus:border-green-600' type="text" name="" id="" />
+                    </div>
+                    
+                    <button className='flex justify-center items-center gap-2 border-2 border-x-green-700 hover:border-y-green-700 px-4 py-2 rounded-full hover:bg-green-400 w-fit bg-green-600 transition-all ease-in-out' ><lord-icon
+                        src="https://cdn.lordicon.com/jgnvfzqg.json"
+                        trigger="hover"
+                    >
+                    </lord-icon>Add Password
+                    </button>
+                </div>
             </div>
-            
+
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center bg-purple-400 px-4 h-14'>
-        <div className='logo font-bold'>PassOP</div>
-        <ul className='flex gap-4 '>
-            <li><a className='hover:font-semibold' href='/'>Home</a></li>
-            <li><a className='hover:font-semibold' href='/about'>About</a></li>
-            <li><a className='hover:font-semibold' href='/contact'>Contact</a></li>
+    <nav className='flex justify-between items-center text-white bg-purple-400 px-4 h-14'>
+      <div className='logo font-bold text-2xl'>
+        <span className='text-green-700'>&lt;</span>
+        Pass
+        <span className='text-green-700'>OP/&gt;</span>
+      </div>
+      <ul className='flex gap-4 '>
+        <li><a className='hover:font-semibold' href='/'>Home</a></li>
+        <li><a className='hover:font-semibold' href='/about'>About</a></li>
+        <li><a className='hover:font-semibold' href='/contact'>Contact</a></li>
 
-        </ul>
+      </ul>
 
     </nav>
   )
